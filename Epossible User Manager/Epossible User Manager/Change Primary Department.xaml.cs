caa1d9461b1f4295a7256a4265abf965dabbcdc9
mansouriamn <38742344+mansouriamn@users.noreply.github.com>
2018-08-10 12:58:15 -0700

    This is the first commit to development branch

---
+++ Epossible User Manager/Epossible User Manager/Change Primary Department.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Epossible_User_Manager
+{
+    /// <summary>
+    /// Interaction logic for Change_Primary_Department.xaml
+    /// </summary>
+    public partial class Change_Primary_Department : Window
+    {
+        public Change_Primary_Department()
+        {
+            InitializeComponent();
+        }
+    }
+}
